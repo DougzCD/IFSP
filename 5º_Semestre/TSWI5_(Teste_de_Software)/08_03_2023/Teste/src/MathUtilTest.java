fb21be59484a183f57b19e7c15c6a1cc80802445
DougzCD <110394134+DougzCD@users.noreply.github.com>
2023-03-08 14:08:16 +0000

    sei la

--- 5º_Semestre/TSWI5_(Teste_de_Software)/08_03_2023/Teste/src/MathUtilTest.java
+++ 5º_Semestre/TSWI5_(Teste_de_Software)/08_03_2023/Teste/src/MathUtilTest.java
@@ -14,7 +14,7 @@ public class MathUtilTest {
     public void testMdc() {
         int a = 6;
         int b = 3;
-        int esperado = 3;
+        int esperado = 2;
         int obtido = MathUtil.mdc(a, b);
         assertEquals(esperado,obtido);
     }}
